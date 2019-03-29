71a197a8aff4753f80aa2e310172c20c4dc5266d
Ivan Nomokonov <cpwxt38@dev.vtb>
2019-03-29 16:46:51 +0300

    menu

--- KR/Form1.Designer.cs
+++ KR/Form1.Designer.cs
@@ -28,13 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.автомобилиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отзывыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.личныйКабинетToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.автомобилиToolStripMenuItem,
+            this.отзывыToolStripMenuItem,
+            this.личныйКабинетToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // автомобилиToolStripMenuItem
+            // 
+            this.автомобилиToolStripMenuItem.Name = "автомобилиToolStripMenuItem";
+            this.автомобилиToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.автомобилиToolStripMenuItem.Text = "Автомобили";
+            // 
+            // отзывыToolStripMenuItem
+            // 
+            this.отзывыToolStripMenuItem.Name = "отзывыToolStripMenuItem";
+            this.отзывыToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.отзывыToolStripMenuItem.Text = "Отзывы";
+            // 
+            // личныйКабинетToolStripMenuItem
+            // 
+            this.личныйКабинетToolStripMenuItem.Name = "личныйКабинетToolStripMenuItem";
+            this.личныйКабинетToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.личныйКабинетToolStripMenuItem.Text = "Личный кабинет";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem автомобилиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отзывыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem личныйКабинетToolStripMenuItem;
     }
 }
 
